2e83bd2499b54d567d628cad49981ecd7846dd5e
Krzysztof <59841838+qaletka@users.noreply.github.com>
2024-05-12 23:07:17 +0200

    Create test_duckduckgo.py

---
+++ tests/test_duckduckgo.py
@@ -0,0 +1,14 @@
+from selenium.webdriver import Chrome
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+def test_searching_in_duckduckgo():
+    # Uruchomienie przeglądarki Chrome. Ścieżka do chromedrivera
+    # ustawiana automatycznie przez bibliotekę webdriver-manager
+    browser = Chrome(executable_path=ChromeDriverManager().install())
+    # Otwarcie strony duckduckgo
+    # Znalezienie paska wyszukiwania
+    # Znalezienie guzika wyszukiwania (lupki)
+    # Asercje że elementy są widoczne dla użytkownika
+    # Szukanie Vistula University
+    # Sprawdzenie że pierwszy wynik ma tytuł 'Vistula University in Warsaw'
+    # Zamknięcie przeglądarki
